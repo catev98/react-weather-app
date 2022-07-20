270f0c12cb1d1a3a3dca7699243de887a998b488
catev98 <vicente.bcatarina@gmail.com>
2022-07-20 19:02:09 +0100

    add git url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Catarina Vicente and is
+        <a href="https://github.com/catev98/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
