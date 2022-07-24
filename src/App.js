5e0f18b9015de9a05b5ec7aa14490ffba45c7e05
catev98 <vicente.bcatarina@gmail.com>
2022-07-24 21:30:55 +0100

    html submit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by Catarina Vicente and is
-        <a href="https://github.com/catev98/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project is coded by Catarina Vicente and is{" "}
+          <a href="https://github.com/catev98/react-weather-app">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
