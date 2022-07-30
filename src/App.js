a7c17f0157b608a0bb62016692aeead1531da133
catev98 <vicente.bcatarina@gmail.com>
2022-07-30 14:33:28 +0100

    icon api

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        <Weather defaultCity="New York"/>
+        <Weather defaultCity="New York" />
         <footer>
           This project is coded by Catarina Vicente and is{" "}
           <a href="https://github.com/catev98/react-weather-app">
